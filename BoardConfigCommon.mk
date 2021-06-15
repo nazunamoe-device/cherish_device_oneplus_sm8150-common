3567833e421e6ffa622134a61c6af5fb3c14cf2b
John Galt <johngaltfirstrun@gmail.com>
2021-06-15 11:03:03 -0400

    BoardConfigCommon/kernel: set LLVM=1 since llvm tools are in use
    In the future, this will be used to set all llvm tools for easier
    testing, and the possible treewide changes to support full llvm tools.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
-TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
